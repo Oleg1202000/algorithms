0bf79d2c4eee7ba0017fd01d7de60ff53bd5cea5
Oleg1202000 <Oleg1202000@gmail.com>
2023-06-28 00:10:07 +0300

    solved

--- main.py
+++ main.py
@@ -1,37 +1,22 @@
-# https://leetcode.com/problems/merge-sorted-array/?envType=study-plan-v2&envId=top-interview-150
+# https://leetcode.com/problems/remove-element/submissions/981153793/?envType=study-plan-v2&envId=top-interview-150
 
 from typing import List
 
+
 class Solution:
-    def merge(nums1: List[int], m: int, nums2: List[int], n: int) -> List[int]:
-        index_1 = m - 1
-        index_2 = n - 1
+    def removeElement(nums: List[int], val: int) -> int:
 
-        while True:
-            if index_1 < 0:
-                nums1[0:index_1+index_2+2] = nums2[0:index_2+1]
-                break
-            elif index_2 < 0:
-                nums1[0:index_1+index_2+2] = nums1[0:index_1+1]
-                break
-
-            if nums1[index_1] > nums2[index_2]:
-                nums1[index_1+index_2+1] = nums1[index_1]
-                index_1 -= 1
-            elif nums1[index_1] < nums2[index_2]:
-                nums1[index_1+index_2+1] = nums2[index_2]
-                index_2 -= 1
+        k = 0
+        for _ in range(len(nums)):
+            if nums[k] == val:
+                nums.pop(k)
             else:
-                nums1[index_1+index_2+1] = nums1[index_1]
-                nums1[index_1+index_2] = nums2[index_2]
-                index_1 -= 1
-                index_2 -= 1
+                k += 1
 
-        return nums1
+        return k
 
 
-nums1 = list(map(int, input().split(",")))
-m = int(input())
-nums2 = list(map(int, input().split(",")))
-n = int(input())
-print(Solution.merge(nums1=nums1, m=m, nums2=nums2, n=n))
+nums = list(map(int, input().split(",")))
+val = int(input())
+
+print(Solution.removeElement(nums=nums, val=val))
