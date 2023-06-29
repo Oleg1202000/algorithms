d0c584abbd10a676ea3895408a531e6da86f9ea6
Oleg1202000 <Oleg1202000@gmail.com>
2023-06-29 21:26:44 +0300

    solved

--- main.py
+++ main.py
@@ -1,24 +1,25 @@
-# https://leetcode.com/problems/remove-element/submissions/981153793/?envType=study-plan-v2&envId=top-interview-150
+# https://leetcode.com/problems/remove-duplicates-from-sorted-array/?envType=study-plan-v2&envId=top-interview-150
 
 from typing import List
 
 
 class Solution:
-    def removeElement(nums: List[int], val: int) -> int:
+    def removeDuplicates(nums: List[int]) -> int:
 
         k = 0
-        second_index = len(nums) - 1
-        for _ in range(len(nums)):
-            if nums[k] == val:
-                nums[k], nums[second_index] = nums[second_index], nums[k]
-                second_index -= 1
-            else:
+        second_index = 1
+        while second_index != len(nums):
+            if nums[k] < nums[second_index]:
+                nums[k+1], nums[second_index] = nums[second_index], nums[k+1]
                 k += 1
+            else:
+                second_index += 1
 
-        return k
+        print(nums)
+
+        return k + 1
 
 
 nums = list(map(int, input().split(",")))
-val = int(input())
 
-print(Solution.removeElement(nums=nums, val=val))
+print(Solution.removeDuplicates(nums=nums))
