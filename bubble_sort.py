94319e566e9729f55fd0ec6479f30ff9a056077f
mk1MoreBugs <o.v.klevtsov@ya.ru>
2024-04-22 12:11:46 +0300

    detele old python files

--- bubble_sort.py
+++
@@ -1,9 +0,0 @@
-def bubble_sort(lst):
-
-    for i in range(len(lst)-1):
-        for j in range(i+1, len(lst)):
-            if lst[i] > lst[j]:
-                lst[i], lst[j] = lst[j], lst[i]
-
-    return lst
-
